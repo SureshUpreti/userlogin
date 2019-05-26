9e6d9b4717a9628b2e6f4684f2ccf8f228fc3d54
SureshUpreti <ricardosuresh@gmail.com>
2019-05-26 14:37:51 +0545

    modified sql query on config.php and process.php files

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'logintest');
-$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+$conn = mysql_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
--- process.php
+++ process.php
@@ -4,13 +4,13 @@
    
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       
-      $myusername = mysqli_real_escape_string($conn,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
+      $myusername = mysql_real_escape_string($conn,$_POST['username']);
+      $mypassword = mysql_real_escape_string($conn,$_POST['password']); 
       
       $sql = "SELECT id FROM users WHERE username = '$myusername' and password = '$mypassword'";
-      $result = mysqli_query($db,$sql);
+      $result = mysql_query($db,$sql);
       
-      $count = mysqli_num_rows($result);
+      $count = mysql_num_rows($result);
       
 		
       if($count == 1) {
@@ -19,7 +19,7 @@
          
          header("location: welcome.php");
       }else {
-         header("location:invalid.php");
+         header("location: invalid.php");
       }
    }
 ?>
